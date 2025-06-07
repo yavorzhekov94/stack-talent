89b1f8178f951fc110182bb32e2687a1a8378318
Yavor Zhekov <yavor_zhekov@abv.bg>
2025-06-07 23:39:00 +0300

    Frontend - part 4

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,1 @@
-import $ from 'jquery';
-window.$ = window.jQuery = $;
-
-import 'jquery.easing';
-import 'jquery-waypoints/waypoints.min.js';
-import WOW from 'wow.js';
-import 'owl.carousel';
-
-new WOW().init();
-
-import '../scss/app.scss';
-
-import './main.js';
-
 import './bootstrap';
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,8 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    <title>Stack Talents</title>
+    <link rel="stylesheet" href="{{ asset('vendor/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/css/owl.theme.green.min.css') }}">
+    @vite(['resources/scss/app.scss'])
 </head>
 <body>
     <div class="container-xxl bg-white p-0">
@@ -14,5 +19,11 @@
         <x-page-sections.footer />
         <x-page-sections.back-to-top />
     </div>
+    <script src="{{ asset('vendor/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/js/easing.min.js') }}"></script>
+    <script src="{{ asset('vendor/js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('vendor/js/wow.min.js') }}"></script>
+    <script src="{{ asset('vendor/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('vendor/js/main.js') }}"></script>
 </body>
 </html>
